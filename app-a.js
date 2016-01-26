5c48fe79993c2c0925fc91d4c41e1a6592edfa82
Laura Pigott <pigottlaura@gmail.com>
2016-01-26 19:08:17 +0000

    Adding ability to ignore files
    When I created the app-b and ran it I realised that the contents of app-a.js, as well as .atom-ternjs, were now printing out the the console. To fix this using my current method (i.e of checking whether the filename was equal to app.a.js) I would have had to extend the if statement significantly. Instead, I have created an ignoreFiles array, which contains the strings of the filetypes I would like to ignore. I then passed a checkIgnore() function into the if statement, and used it to return true or false, based on whether part of the filename of the file that is currently being checked contains any of the strings I am choosing to ignore i.e. .js and .atom-ternjs

--- app-a.js
+++ app-a.js
@@ -5,6 +5,16 @@
 // been read (see filesReturned function)
 var allFiles = [];
 
+// Creating an ignoreFiles array, where I can list the filetypes (or
+// filenames) which I don't wish to store the details of i.e. their
+// content and filename. I am doing this because any .js files in this
+// folder contain alot of code, and I do not want to be logging this
+// out everytime I run the app
+var ignoreFiles = [
+  ".js",
+  ".tern-project"
+]
+
 // Creating a counter to monitor the current value of, and itterate
 // through each of the files found in the directory
 var counter = 0;
@@ -42,7 +52,7 @@ function filesReturned(err, fileDir){
       // Checking to make sure the current file is not called "app.js" i.e.
       // I do not want to store the contents of this file, just the ones
       // that exist in the same directory as it
-      if(fileDir[counter] != "app.js")
+      if(checkIgnoreFiles(fileDir[counter]))
       {
         // Creating a new object to store the information of this file temporarily
         var thisFile = {
@@ -77,7 +87,7 @@ function filesReturned(err, fileDir){
         // All of the files have been returned, and therefore all of their information
         // has been stored into the allFiles array. Note - none of the file's data has
         // been logged out to the console yet. It has just been stored in the allFiles array
-        console.log("All files have been read, and their data stored");
+        console.log("All files have been read, and their data stored\nNote - some files may have been ignored due to your current settings");
 
         allFilesReturned();
       }
@@ -94,4 +104,27 @@ function allFilesReturned(){
     // within an object, in the allFiles array
     console.log("\t- The contents of " + allFiles[i].name + " was \"" + allFiles[i].contents + "\"");
   }
+}
+
+function checkIgnoreFiles(_filename){
+  // Creating an answer variable, which is automatically true i.e. I am assuming that
+  // the file names aren't going to be ignored until proved otherwise
+  var ans = true;
+
+  // Looping through each of the filetypes (or names) that I have stored in the ignoreFiles array,
+  for(f in ignoreFiles){
+    // Checking if the indexOf the filetype (or name) has an index within the string of the current
+    // filename i.e. does it have an index of 0 or more (anything below this does not exist within
+    // the _filename string)
+    if(_filename.indexOf(ignoreFiles[f]) > -1)
+    {
+      // If this string contains a name or filetype which I have added to the ignoreFiles array, then
+      // change the answer to false
+      ans = false;
+    }
+  }
+  // Return true or false, depending on if this filename contains, or is, a file which we have chosen to
+  // ignore i.e. if this function returns true, then the files name and contents will be saved, whereas
+  // if it returns false, the file details will not be stored
+  return ans;
 }
