e528270ff0a37943a78c1aee6f377ed83f4358e2
Laura Pigott <pigottlaura@gmail.com>
2016-01-26 19:45:08 +0000

    Looping through data - Synchronous
    Using the js.readFileSync() function to allow me to read in files while looping through an array. Ran into one unexpected error with this one. As soon as I tried to run the app I was getting the error: 'fs.js:651 var r = binding.read(fd, buffer, offset, length, position); ^ Error: EISDIR: illegal operation on a directory, read'. Previously, using js.readFile() Asynchronous method the .git filename was returning 'The contents of .git was 'undefined'. It is interesting to note this difference between these otherwise similar methods, as js.readFile() seems ok with just returning undefined if pass in a directory, whereas js.readFileSync() throws an error

--- app-b.js
+++ app-b.js
@@ -12,7 +12,8 @@ var allFiles = [];
 // out everytime I run the app
 var ignoreFiles = [
   ".js",
-  ".tern-project"
+  ".tern-project",
+  ".git"
 ]
 
 // Creating a counter to monitor the current value of, and itterate
@@ -34,65 +35,40 @@ function filesReturned(err, fileDir){
   // Logging out a message so I know when the data has been returned
   console.log("\nData returned from Directory \n");
 
-  // Calling the readMyFiles() function, which is declared below,
+  // Looping through the contents of the fileDir parametre
   // so that all of the files that were found in my directory can
   // be read, and their data stored in a global array
-  readMyFiles();
+  for(i in fileDir)
+  {
+    // Checking to make sure the current file is not called "app.js" i.e.
+    // I do not want to store the contents of this file, just the ones
+    // that exist in the same directory as it
+    if(checkIgnoreFiles(fileDir[i]))
+    {
+      // Creating a new object to store the information of this file temporarily
+      var thisFile = {
+        // Getting the name of the file from the file directory results, again
+        // using the counter to identify to it
+        name:fileDir[i],
+        // Passing in a file from the array returned by the readFileSync() function
+        // to get the contents of the file. Using the counter to itterate through
+        // each of these files in turn.
+        contents: fs.readFileSync(fileDir[i])
+      };
 
-  // Declaring the readMyFiles function so that the code within it
-  // can be recalled to read through multiple files. I am keeping this
-  // function within the filesReturned function as it means the fileDir
-  // (data result of the readdir() request earlier) does not need to be
-  // passed out to a global variable just to be passed back in again
-  function readMyFiles(){
-
-    // Passing in a file from the array returned by the readdir() function.
-    // Using the counter to itterate through each of these files.
-    fs.readFile(fileDir[counter], {encoding: "utf8"}, function(err, result){
-      // Checking to make sure the current file is not called "app.js" i.e.
-      // I do not want to store the contents of this file, just the ones
-      // that exist in the same directory as it
-      if(checkIgnoreFiles(fileDir[counter]))
-      {
-        // Creating a new object to store the information of this file temporarily
-        var thisFile = {
-          // Getting the name of the file from the file directory results, again
-          // using the counter to identify to it
-          name:fileDir[counter],
-          // Getting the contents of the file from the parametre "result" that
-          // was passed back to the readFile() function callback
-          contents:result
-        };
-
-        // Pushing this temporary object into the allFiles array, so that it
-        // can be stored and accessed later on i.e. once all files have been
-        // returned
-        allFiles.push(thisFile);
-      }
-
-      // Increasing the counter value, so that the next time this function
-      // is called, the next file can be accessed from the fileDir array
-      counter++;
-
-      // Checking if the counter is still less than the length of the fileDir
-      // array. If it is, then there are still files to be read, so it should
-      // go back and read more files. If it is now equal to (or greater than
-      // - which it should never be) then all the files from the current
-      // directory have been read (and their data stored), so we now want
-      // to call the filesReturned() function
-      if(counter < fileDir.length)
-      {
-        readMyFiles();
-      }else{
-        // All of the files have been returned, and therefore all of their information
-        // has been stored into the allFiles array. Note - none of the file's data has
-        // been logged out to the console yet. It has just been stored in the allFiles array
-        console.log("All files have been read, and their data stored\nNote - some files may have been ignored due to your current settings");
-
-        allFilesReturned();
-      }
-    });
+      // Pushing this temporary object into the allFiles array, so that it
+      // can be stored and accessed later on i.e. once all files have been
+      // returned
+      allFiles.push(thisFile);
+    }
   }
+
+  // All of the files have been returned, and therefore all of their information
+  // has been stored into the allFiles array. Note - none of the file's data has
+  // been logged out to the console yet. It has just been stored in the allFiles array
+  console.log("All files have been read, and their data stored\nNote - some files may have been ignored due to your current settings");
+
+  allFilesReturned();
 }
 
 function allFilesReturned(){
