fbccecb2a7779324320de377bfe03790cac0adab
Laura Pigott <pigottlaura@gmail.com>
2016-01-26 14:07:48 +0000

    Not loggin out current file
    Logging out all the file names from the current directory, except for the current file (using the actual filename. Will look for a way to do this so that even if the file name changes, this code will still work.

--- app.js
+++ app.js
@@ -6,12 +6,25 @@ var fs = require("fs");
 // once they get returned
 fs.readdir("./", filesReturned);
 
+// Creating the callback function for the readdir() request above
+// which takes in two parametres - any errors, and the files or
+// result returned
 function filesReturned(err, files){
+  // Logging out a message so I know when the files have been returned
   console.log("Files returned from Directory");
 
+  // Looping through each file in the result
   for(f in files)
   {
-    console.log(files[f]);
+    // Checking if the current filename is equal to this
+    // file's name. If it is, then is doesn't get printed out
+    // (Technically this is a cheat, will look at the api
+    // to find a better way to test if it is the current file)
+    if(files[f] !== "app.js")
+    {
+      // Logging out the file name for each file
+      console.log(files[f]);
+    }
   }
 
 }
