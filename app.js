f781736f5307f042fad528769cf2bda104ddd20c
Laura Pigott <pigottlaura@gmail.com>
2016-01-26 13:56:46 +0000

    Reading files from the current directory
    Reading files from the directory the app is in, and logging out to the console as soon as they are returned

---
+++ app.js
@@ -0,0 +1,11 @@
+// Requiring the File System module
+var fs = require("fs");
+
+// Using the File System module to read the current
+// directory I am in, and calling the filesReturned callback
+// once they get returned
+fs.readdir("./", filesReturned);
+
+function filesReturned(err, files){
+  console.log("Files returned from Directory");
+}
